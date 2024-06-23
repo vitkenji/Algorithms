5c685505e233ce869d28e13bde6b74eb15ef0d94
vkzy <vkzya@LAPTOP-V2LRBOMP>
2024-06-23 10:31:08 -0300

    better graph example for floydWarshall

--- floydWarshall/main.c
+++ floydWarshall/main.c
@@ -2,6 +2,7 @@
 
 int main()
 {
+    /*
     MGraph* G = createMGraph(5);
     insertWeightEdgeMatrixDirectional( G, 0, 1, 4);
     insertWeightEdgeMatrixDirectional( G, 0, 2, 2);
@@ -12,6 +13,18 @@ int main()
     insertWeightEdgeMatrixDirectional( G, 2, 4, 5);
     insertWeightEdgeMatrixDirectional( G, 2, 1, 1);
     insertWeightEdgeMatrixDirectional( G, 4, 3, 1);
+    printMGraph(G, 5);*/
+    MGraph* G = createMGraph(5);
+    insertWeightEdgeMatrixDirectional( G, 0, 1, 4);
+    insertWeightEdgeMatrixDirectional( G, 0, 3, 5);
+    insertWeightEdgeMatrixDirectional( G, 1, 4, 6);
+    insertWeightEdgeMatrixDirectional( G, 1, 2, 1);
+    insertWeightEdgeMatrixDirectional( G, 2, 0, 2);
+    insertWeightEdgeMatrixDirectional( G, 2, 3, 3);
+    insertWeightEdgeMatrixDirectional( G, 3, 2, 1);
+    insertWeightEdgeMatrixDirectional( G, 3, 4, 2);
+    insertWeightEdgeMatrixDirectional( G, 4, 3, 4);
+    insertWeightEdgeMatrixDirectional( G, 4, 0, 1);
     printMGraph(G, 5);
     printf("\n");
 
