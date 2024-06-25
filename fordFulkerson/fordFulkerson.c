a46a10f2e8ef4a4e62cfd846abbddbef22912ded
vitkenji <vkz.yamada@gmail.com>
2024-06-25 11:58:54 -0300

    creating example graph, creating residual graph

--- fordFulkerson/fordFulkerson.c
+++ fordFulkerson/fordFulkerson.c
@@ -2,5 +2,7 @@
 
 int fordFulkerson(LGraph* G, int s, int t)
 {
-    
+    LGraph* Gr = createLGraph(G->V);
+    int f = 0;
+    return f;
 }
--- fordFulkerson/main.c
+++ fordFulkerson/main.c
@@ -2,5 +2,13 @@
 
 int main()
 {
+    LGraph* G = createLGraph(4);
+    InsertWeightEdgeListDirectional(G, 0, 1, 3);
+    InsertWeightEdgeListDirectional(G, 0, 2, 2);
+    InsertWeightEdgeListDirectional(G, 1, 3, 2);
+    InsertWeightEdgeListDirectional(G, 2, 3, 3);
+    InsertWeightEdgeListDirectional(G, 1, 2, 5);
+    printLGraph(G, 4);
+    printf("%d ", fordFulkerson(G, 0, 3));
     return 0;
 }
